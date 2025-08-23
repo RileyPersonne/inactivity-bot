57169e0aacf06f94bd7c7adf8df03905b512291f
Riley Personne <dwalton589@gmail.com>
2025-08-23 14:02:18 -0700

    fixed code

--- bot.py
+++ bot.py
@@ -23,35 +23,37 @@ async def on_message(message):
         await message.channel.send('Arf!')
 
     if message.content.startswith('$activity') and message.author.guild_permissions.administrator:
-        guild = client.guilds[0]
-        table = [[0 for x in range(2)] for y in range(guild.member_count)]
+        guild = client.get_guild(message.guild.id)
+        print(guild.name + " " + str(guild.member_count))
+
+        table = [[0, datetime.datetime(1,1,1).date()] for y in range(guild.member_count)]
         i = 0
-        oldest_message = datetime.datetime(9999,9999,9999).date()
+        oldest_message = datetime.datetime(9999,12,31).date()
 
         for member in guild.members:
-            table[0][i] = member.name
-            table[1][i] = datetime.datetime(1,1,1).date()
+            print(str(i) + " " + member.name)
+            table[i][0] = member.name
             i = i + 1
 
         for channel in guild.text_channels:
             limit=250
-            if channel.id == 1236856577596719138:
+            if channel.name == "general":
                 limit=6000
-            async for message in channel.history(limit=limit):
+            async for post in channel.history(limit=limit):
                 for i in range(guild.member_count):
-                    if table[0][i] == message.author.name and table[1][i] < message.created_at.date():
-                            table[1][i] = message.created_at.date()
-                if limit==6000 and message.create_at.date() < oldest_message:
-                    oldest_message = message.created_at.date()
+                    if table[i][0] == post.author.name and table[i][1] < post.created_at.date():
+                            table[i][1] = post.created_at.date()
+                if limit==6000 and post.created_at.date() < oldest_message:
+                    oldest_message = post.created_at.date()
 
         with (open("table.txt", "w") as file):
             i = 0
             file.write(f'oldest message surveyed in general: {oldest_message}\n')
             for row in table:
-                if table[1][i] == oldest_date:
-                    file.write(f'{table[0][i]} : prior to last surveyed message\n')
+                if table[i][1] == oldest_message:
+                    file.write(f'{table[i][0]} : prior to last surveyed message\n')
                 else:
-                    file.write(f'{table[0][i]} : {(datetime.datetime.today().date()-table[1][i]).days}\n')
+                    file.write(f'{table[i][0]} : {(datetime.datetime.today().date()-table[i][1]).days}\n')
                 i = i + 1
 
         dm = await client.create_dm(message.author)
