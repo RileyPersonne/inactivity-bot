134154af142c6c5784596d347d365d83d1731317
Valerie Walton <57420333+RileyPersonne@users.noreply.github.com>
2025-08-23 11:07:35 -0700

    Update bot.py
    Updated search parameters

--- bot.py
+++ bot.py
@@ -26,6 +26,7 @@ async def on_message(message):
         guild = client.guilds[0]
         table = [[0 for x in range(2)] for y in range(guild.member_count)]
         i = 0
+        oldest_message = datetime.datetime(9999,9999,9999).date()
 
         for member in guild.members:
             table[0][i] = member.name
@@ -33,15 +34,24 @@ async def on_message(message):
             i = i + 1
 
         for channel in guild.text_channels:
-            async for message in channel.history(limit=500):
+            limit=250
+            if channel.id = 1236856577596719138:
+                limit=6000
+            async for message in channel.history(limit=limit):
                 for i in range(guild.member_count):
                     if table[0][i] == message.author.name and table[1][i] < message.created_at.date():
                             table[1][i] = message.created_at.date()
+                if limit==6000 and message.create_at.date() < oldest_message:
+                    oldest_message = message.created_at.date()
 
         with (open("table.txt", "w") as file):
             i = 0
+            file.write(f'oldest message surveyed in general: {oldest_message}\n')
             for row in table:
-                file.write(f'{table[0][i]} : {(datetime.datetime.today().date()-table[1][i]).days}\n')
+                if table[1][i] == oldest_date:
+                    file.write(f'{table[0][i]} : prior to last surveyed message\n')
+                else:
+                    file.write(f'{table[0][i]} : {(datetime.datetime.today().date()-table[1][i]).days}\n')
                 i = i + 1
 
         dm = await client.create_dm(message.author)
