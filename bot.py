f46ed2713a7e9b28b7f33068950705e3138336b7
Riley Personne <dwalton589@gmail.com>
2025-08-26 09:16:30 -0700

    refactored code to be more inline with modern standards

--- bot.py
+++ bot.py
@@ -4,27 +4,31 @@ import asyncio
 
 import activity
 
-intents = discord.Intents.default()
-intents.messages = True
-intents.message_content = True
-intents.members = True
+def main():
+    intents = discord.Intents.default()
+    intents.messages = True
+    intents.message_content = True
+    intents.members = True
 
-client = discord.Client(intents=intents)
+    client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+    @client.event
+    async def on_ready():
+        print(f'We have logged in as {client.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
 
-    if message.content.startswith('$hello Valbot'):
-        await message.channel.send('Arf!')
+        if message.content.startswith('$hello Valbot'):
+            await message.channel.send('Arf!')
 
-    if message.content.startswith('$activity') and message.author.guild_permissions.administrator:
-        asyncio.create_task(activity.command_activity(message, client))
+        if message.content.startswith('$activity') and message.author.guild_permissions.administrator:
+            asyncio.create_task(activity.command_activity(message, client))
 
 
-client.run(os.getenv('TOKEN'))
+    client.run(os.getenv('TOKEN'))
+
+if __name__ == "__main__":
+  main()
